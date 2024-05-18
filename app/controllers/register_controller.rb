05dd5132c25beec30f3727cc8599c3da4f5e5139
DimonKachek <login0955@gmail.com>
2024-05-18 08:35:53 +0300

    Add Sing up/in/out for students and teachers

---
+++ app/controllers/register_controller.rb
@@ -0,0 +1,5 @@
+class RegisterController < ApplicationController
+	def new
+		render
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :teachers
   devise_for :students
+
+  get 'login', to: 'login#new'
+  get 'register', to: 'register#new'
+
   root to: "home#index"
 end
