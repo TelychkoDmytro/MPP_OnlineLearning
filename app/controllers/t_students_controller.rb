7762f796ed393c6d4b5b61e97589a71edcb0c9e6
DimonKachek <login0955@gmail.com>
2024-05-20 11:06:51 +0300

    Change paths to t_student

--- app/controllers/t_students_controller.rb
+++ app/controllers/t_students_controller.rb
@@ -1,6 +1,7 @@
 class TStudentsController < ApplicationController
-  before_action :authenticate_teacher!, except: [:index, :show]
+  before_action :authenticate_teacher!, except: [:edit, :index, :show]
   before_action :set_student, only: [:edit, :update]
+  before_action :check_access_for_edit, only: [:edit]
 
   def index
     # @students = Student.where(group_id: nil)
@@ -14,16 +15,20 @@ class TStudentsController < ApplicationController
         @student = Student.find(params[:id])
       end
     else
-      puts 
+      puts "Acces denied"
     end
   end
 
   def edit
+    unless current_student.nil?
+      flash[:notice] = "Access denied"
+      redirect_to root_path
+    end
   end
 
   def update
     if @student.update(student_params)
-      redirect_to students_path, notice: 'Student was successfully updated.'
+      redirect_to t_students_path, notice: 'Student was successfully updated.'
     else
       render :edit
     end
@@ -37,5 +42,19 @@ class TStudentsController < ApplicationController
 
   def student_params
     params.require(:student).permit(:id, :first_name, :last_name, :group_id)
+  end
+
+  def check_access_for_edit
+    if student_signed_in?
+      flash[:notice] = "Access denied"
+      redirect_to t_students_path
+    elsif teacher_signed_in?
+      # Allow access to the edit action if the teacher is signed in
+      authenticate_teacher!
+    else
+      # Handle the case where no user is signed in
+      flash[:alert] = "You need to sign in as a teacher to access this page"
+      redirect_to new_teacher_session_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get 'schedules/index'
+  # get 'schedules/show'
+  # get 'schedules/edit'
+  # get 'schedules/new'
   # get 'students/index'
   # get 'students/show'
   # get 'students/edit'
@@ -6,11 +10,12 @@ Rails.application.routes.draw do
   # get 'groups/new'
   # get 'groups/create'
   # get 'groups/show'
-  resources :students
+  resources :schedules
+  resources :t_students
   resources :groups
   resources :subjects
 
-  devise_for :teachers
+  devise_for :teachers, path: 'teachers'
   devise_for :students
 
   get 'login', to: 'login#new'
